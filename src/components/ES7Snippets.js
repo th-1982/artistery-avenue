ff2f7b99e42b2e5d178a00e7a9e738070cd8ba3f
Theresa Wolff <theresawolff4@gmail.com>
2024-01-30 23:43:26 +0000

    Add NavBar component

---
+++ src/components/ES7Snippets.js
@@ -0,0 +1,47 @@
+// import React from 'react'
+
+// function ES7Snippets() {
+//  return (
+//    <div>ES7Snippets</div>
+//  )
+// }
+
+// export default ES7Snippets
+
+
+// import React from 'react'
+
+// const ES7Snippets = () => {
+//  return (
+//    <div>ES7Snippets</div>
+//  )
+// }
+
+// export default ES7Snippets
+
+// import React, { Component } from 'react'
+
+
+// export class ES7Snippets extends Component {
+//  render() {
+//    return (
+//      <div>ES7Snippets</div>
+//    )
+//  }
+// }
+
+// export default ES7Snippets
+
+
+// constructor(props) {
+//  super(props)
+
+//  this.state = {
+//     first
+//  }
+// }
+
+
+// console.log(first)     
+
+// console.log('first', first)
