eedc8ad0f670d90e429aea5ab909e4ec7136baf3
Theresa Wolff <theresawolff4@gmail.com>
2024-02-17 01:27:46 +0000

    Add page not found component

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import ProfilePage from "./pages/profiles/ProfilePage";
 import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
+import NotFound from "./components/NotFound";
 
 
 function App() {
@@ -78,7 +79,7 @@ function App() {
         render={() => <ProfileEditForm />}
       />
 
-      <Route render={() => <p>Page not found!</p>} />
+      <Route render={() => <NotFound />} />
       </Switch> 
      </Container>
     </div>
